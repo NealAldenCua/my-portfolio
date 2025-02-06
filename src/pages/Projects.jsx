325bf3de307be572be2fdca318fcdb039da4308e
Neal Alden Cua <167517253+NealAldenCua@users.noreply.github.com>
2025-02-06 13:48:36 +0800

    revised resume

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,16 +1,52 @@
 import "react";
 import mobile from '../assets/images/mobile.png';
 import web from '../assets/images/web.jpg';
+import infor from '..//assets/images/infor.png';
+import icreate from '../assets/images/icreate.png';
 
 import { SiArduino } from "react-icons/si";
 import { FaJava } from "react-icons/fa";
 import { IoLogoFirebase } from "react-icons/io5";
 import { FaReact } from "react-icons/fa";
+import { SiSelenium } from "react-icons/si";
+import { TbBrandMinecraft } from "react-icons/tb";
 
 const Projects = () =>{
     return(
         <section>
-        <h1>Projects | Experiences</h1>
+        <h1>Experiences</h1>
+        <div className="project-container">
+            <div className="project-card">
+                <img src={infor} alt="exp1"/>
+                <div className="project-info">
+                    <h2>Infor PSSC Inc. | BGC Taguig</h2>
+                    <div className="project-icons">
+                        <SiSelenium />
+                        <FaJava />
+                    </div>
+                    <p>Worked as a QA Engineer Intern <span>[09/2022 - 04/2023]</span>. Developed and executed comprehensive test cases for software applications.
+                     Has the change to learn some automation. Created detailed documentation to guide the automation setup process</p>
+                    <a href="#">Read More</a>
+                </div>
+            </div>
+
+            <div className="project-card">
+                <img src={icreate} alt="exp2"/>
+                <div className="project-info">
+                    <h1> iCreate Cafe | Fisher Mall, Quezon City</h1>
+                    <div className="project-icons">
+                        <TbBrandMinecraft />
+                    </div>
+                    <p>An internsip during SHS <span>[03/2019 - 05/2019]</span> where I and other shs members developed educational code in Minecraft Education, engaging young
+                    students in coding.</p>
+                    <a href="#">Read More</a>
+                </div>
+            </div>
+
+        </div>
+
+        <h1>Projects</h1>
+
         <div className="project-container">
             <div className="project-card">
                 <img src={mobile} alt="proj1"/>
@@ -46,7 +82,7 @@ const Projects = () =>{
             <div className="project-card">
                 <img src={web} alt="proj3"/>
                 <div className="project-info">
-                    <h1>BillofMaterials</h1>
+                    <h1>TechGizmo</h1>
                     <div className="project-icons">
                         <FaReact />
                         <IoLogoFirebase />
